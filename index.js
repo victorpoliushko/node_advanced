b408bce69727ab7723f8d99d8a407cb8fa91aa22
viktor poliushko <viktor.poliushko@workrise.com>
2022-01-07 23:03:51 +0200

    forking children

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while(Date.now() - start < duration) {}
+//Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but in child mode
+  cluster.fork();
+} else {
+  // Im a child
+  const express = require('express');
+  const app = express();
+  
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {}
+  }
+  
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  });
+  
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there');
-});
-
-app.listen(3000);
