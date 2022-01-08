c5c175c934ffce4f69df5d4c04aef994875a1a1d
viktor poliushko <viktor.poliushko@workrise.com>
2022-01-09 01:02:52 +0200

    preparation for pm2

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-const cluster = require('cluster');
+// Im a child
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
 
-//Is the file being executed in master mode?
-if (cluster.isMaster) {
-  // Cause index.js to be executed *again* but in child mode
-  cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
-} else {
-  // Im a child
-  const express = require('express');
-  const app = express();
-  
-  function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
-  }
-  
-  app.get('/', (req, res) => {
-    doWork(5000);
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     res.send('Hi there');
   });
+});
 
-  app.get('/fast', (req, res) => {
-    res.send('This was fast');
-  });
-  
-  app.listen(3000);
-}
+app.get('/fast', (req, res) => {
+  res.send('This was fast');
+});
+
+app.listen(3000);
